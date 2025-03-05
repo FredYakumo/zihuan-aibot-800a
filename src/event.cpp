b557f2f7e88032ee06d919a0e35453befee94258
fredyakumo <fredyakumo@gmail.com>
2025-03-05 18:30:14 +0800

    Add filtter

--- src/event.cpp
+++ src/event.cpp
@@ -4,6 +4,9 @@
 void AIBot::onEnable() {
     MiraiCP::Event::registerEvent<MiraiCP::GroupMessageEvent>([] (MiraiCP::GroupMessageEvent e) {
         MiraiCP::Logger::logger.info(":)");
-        e.group.sendMessage("baka");
+        auto msg = e.message.toString();
+        if (msg.find("@2496875785") != std::string::npos) {
+            e.group.sendMessage("baka");
+        }
     });
 }
