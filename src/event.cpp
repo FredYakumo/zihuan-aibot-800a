c1c77d08617030d242e633569b7becd6bfb669d5
fredyakumo <fredyakumo@gmail.com>
2025-03-07 16:39:39 +0800

    修复一个运行问题

--- src/event.cpp
+++ src/event.cpp
@@ -1,11 +1,12 @@
 #include "MiraiCP.hpp"
 #include "plugin.h"
+#include <string>
 
 void AIBot::onEnable() {
     MiraiCP::Event::registerEvent<MiraiCP::GroupMessageEvent>([] (MiraiCP::GroupMessageEvent e) {
         MiraiCP::Logger::logger.info("Recv message: " + e.message.toString());
         for (auto msg : e.message) {
-            MiraiCP::Logger::logger.info(std::string("Message Type: ") + msg->getTypeString(msg.getType()) + ", Content: " + msg->content);
+            MiraiCP::Logger::logger.info(std::string("Message Type: ") + std::to_string(msg.getType()) + ", Content: " + msg->content);
         }
 
     });
