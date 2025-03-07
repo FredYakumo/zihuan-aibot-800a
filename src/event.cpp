011af905244203a95f127b1abaa8a0cb137035ca
fredyakumo <fredyakumo@gmail.com>
2025-03-07 17:14:48 +0800

    添加AI总结功能

--- src/event.cpp
+++ src/event.cpp
@@ -5,8 +5,38 @@
 void AIBot::onEnable() {
     MiraiCP::Event::registerEvent<MiraiCP::GroupMessageEvent>([] (MiraiCP::GroupMessageEvent e) {
         MiraiCP::Logger::logger.info("Recv message: " + e.message.toString());
+        MiraiCP::internal::Message *at_me_msg = nullptr;
+        MiraiCP::internal::Message *ref_msg = nullptr;
+        MiraiCP::internal::Message *plain_msg = nullptr;
+
         for (auto msg : e.message) {
             MiraiCP::Logger::logger.info(std::string("Message Type: ") + std::to_string(msg.getType()) + ", Content: " + msg->content);
+
+            if (msg.getType() == MiraiCP::SingleMessageType::At_t && msg->content == std::to_string(e.bot.id())) {
+                at_me_msg = &msg;
+            } else if (msg.getType() == MiraiCP::SingleMessageType::QuoteReply_t) {
+                ref_msg = &msg;
+            } else if (msg.getType() == MiraiCP::SingleMessageType::PlainText_t) {
+                plain_msg = &msg;
+            }
+        }
+
+        if (at_me_msg == nullptr) {
+            return;
+        }
+
+        if (plain_msg->get()->content == "AI总结" || plain_msg->get()->content == "ai总结") {
+            auto msg_chain = MiraiCP::MessageChain{e.sender.at()};
+
+            if (ref_msg == nullptr) {
+                msg_chain.add(MiraiCP::PlainText{std::string{"error: 请引用一个消息."}});
+            } else {
+                msg_chain.add(MiraiCP::PlainText{std::string{"用户让我AI总结消息: \""} + 
+                ref_msg->get()->content + std::string{"\", 但是现在model=DeepSeek-R1:0b"}});
+            }
+
+
+            e.group.sendMessage(msg_chain);
         }
 
     });
