889b0816cabec08f4839daa88e20587d68038e6c
fredyakumo <fredyakumo@gmail.com>
2025-04-07 19:08:00 +0800

    new impl: handle msg event 3

--- include/bot_adapter.h
+++ include/bot_adapter.h
@@ -1,14 +1,14 @@
 #ifndef BOT_ADAPTER_H
 #define BOT_ADAPTER_H
 
+#include "adapter_event.h"
 #include "adapter_message.h"
 #include <easywsclient.hpp>
+#include <functional>
 #include <spdlog/spdlog.h>
 #include <string>
 #include <string_view>
-#include <functional>
 #include <vector>
-#include "adapter_event.h"
 
 namespace bot_adapter {
     class BotAdapter {
@@ -26,12 +26,13 @@ namespace bot_adapter {
             ws = easywsclient::WebSocket::from_url(std::string(url));
         }
         ~BotAdapter();
-        
+
         int start();
 
-        template <typename EventFuncT>
-        inline void register_event(std::function<void(const EventFuncT &e)> func) {
-            msg_handle_func_list.push_back([func] (const MessageEvent &e) {
+        template <typename EventFuncT> inline void register_event(std::function<void(const EventFuncT &e)> func) {
+            static_assert(std::is_base_of<MessageEvent, EventFuncT>::value,
+                          "EventFuncT must be derived from MessageEvent");
+            msg_handle_func_list.push_back([func](const MessageEvent &e) {
                 if (auto specific_event = dynamic_cast<const EventFuncT *>(&e)) {
                     func(*specific_event);
                 }
